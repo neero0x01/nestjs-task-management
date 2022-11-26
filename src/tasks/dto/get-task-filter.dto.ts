d65a649d5f458305ccfc6ebf25cfa900ae8b64fc
Muhammad Ahmad <181370123@gift.edu.pk>
2022-11-26 18:59:39 +0500

    add validations on filter task

--- src/tasks/dto/get-task-filter.dto.ts
+++ src/tasks/dto/get-task-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsOptional, IsEnum, IsString } from 'class-validator';
 import { TasksStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TasksStatus)
   status?: TasksStatus;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
