d535d1acd19c4fd66e851958a5b64eada454dc1e
Muhammad Ahmad <181370123@gift.edu.pk>
2022-11-27 20:01:53 +0500

    define many to one with task entity and one to many with user entity

--- src/auth/entity/user.entity.ts
+++ src/auth/entity/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Task } from '../../tasks/entities/task.entity';
 
 @Entity()
 export class User {
@@ -10,4 +11,7 @@ export class User {
 
   @Column()
   password: string;
+
+  @OneToMany((_type) => Task, (task) => task.user, { eager: true })
+  tasks: Task[];
 }
